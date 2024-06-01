f08acf6f5bcb74bb227792bef5a2d3d6e5c9926c
Evewibe <aleksey.dantist@gmail.com>
2024-06-01 16:20:47 +0600

    feat: get habits endpoint

--- HabitTrackerApp/HabitTrackerAppBackend/Endpoints/HabitsEndpoints/GetHabits.cs
+++ HabitTrackerApp/HabitTrackerAppBackend/Endpoints/HabitsEndpoints/GetHabits.cs
@@ -1,25 +1,34 @@
 ﻿using System.Text.Json.Serialization;
 using Common.Entities;
+using HabitTrackerAppBackend.Infrastructure.Data.Core;
 using HabitTrackerAppBackend.Infrastructure.Endpoints;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 namespace HabitTrackerAppBackend.Endpoints.HabitsEndpoints;
 
 public class GetHabits : EndpointBaseAsync.WithoutRequest.WithActionResult<GetHabitsResponse>
 {
-    public GetHabits()
+    private readonly IAppDbContext _appDbContext;
+
+    public GetHabits(IAppDbContext appDbContext)
     {
-        
+        _appDbContext = appDbContext;
     }
     
     [AllowAnonymous]
     [HttpGet("/habit")]
     public override async Task<ActionResult<GetHabitsResponse>> HandleAsync(CancellationToken cancellationToken = default)
     {
+        var db = _appDbContext.GetDatabase();
+        var collection = db.GetCollection<Habit>("Habits");
+        
+        var habits = await collection.Find(_ => true).ToListAsync(cancellationToken: cancellationToken);
+        
         return Ok(new GetHabitsResponse
         {
-            Habits = new List<Habit>()
+            Habits = habits ?? new List<Habit>()
         });
     }
 }
