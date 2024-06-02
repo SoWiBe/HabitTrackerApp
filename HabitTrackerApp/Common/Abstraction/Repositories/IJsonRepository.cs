3d806b963c3c635b47c47262afaf995775781a16
Evewibe <aleksey.dantist@gmail.com>
2024-06-02 13:34:15 +0600

    feat: common api and json repositories

---
+++ HabitTrackerApp/Common/Abstraction/Repositories/IJsonRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Abstraction.Repositories;
+
+public interface IJsonRepository
+{
+    Task<TResult?> Deserialize<TResult>(string json);
+    Task<string> Serialize(object data);
+}
---
+++ HabitTrackerApp/Common/Repositories/JsonRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Common.Abstraction.Repositories;
+
+namespace Common.Repositories;
+
+public class JsonRepository : IJsonRepository
+{
+    public Task<TResult?> Deserialize<TResult>(string json)
+    {
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        var result = JsonSerializer.Deserialize<TResult>(json, options);
+        return Task.FromResult(result);
+    }
+
+    public Task<string> Serialize(object data)
+    {
+        var result = JsonSerializer.Serialize(data);
+        return Task.FromResult(result);
+    }
+    
+}
